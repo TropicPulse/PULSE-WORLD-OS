959db25b55f756ff8326f97c230861b9b7d2e826
TropicPulse <fordfamilydistribution@gmail.com>
2026-05-01 20:00:42 -0700

    sdfsd

--- PULSE-WORLD/PULSE-UI/PULSEEvolutionaryWindow.js
+++ PULSE-WORLD/PULSE-UI/PULSEEvolutionaryWindow.js
@@ -10,7 +10,7 @@ import { VitalsLogger as PulseLogger }        from "./PulseProofLogger.js";
 import { PageScannerV12 as PULSEOSSkinReflex } from "./PULSEOSSkinReflex.js";
 import PulseChunks from "./PulsePresence-v1.7-Evo.js";
 import PulseUnderstanding from "./PulseUnderstanding.js";
-import { createDualBandOrganism as PulseBinaryOrganismBoot } from "../PULSE-BAND/PULSE-AI/aiDualBand-v11-Evo.js";
+import { createDualBandOrganism as PulseBinaryOrganismBoot } from "./aiDualBand-v11-Evo.js";
 
 import { initUIFlow as PulseUIFlow } from "./PulseUIFlow-v12-EVO.js";
 
